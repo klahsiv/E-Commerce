38782c68a20003ea6b97d93266b991f314692665
Vishal Kumar <vishalkumar2803001@gmail.com>
2022-06-07 17:03:25 +0530

    Created Node Server and some components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,36 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeSceen.js";
 import ProductScreen from "./screens/ProductScreen.js";
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import { LinkContainer } from 'react-router-bootstrap'
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-      <header>
-        <Link to="/">Amazona</Link>
-      </header>
-      <main>
-        <Routes>
-          <Route path="/product/:slug" element = { <ProductScreen /> }></Route>
-          <Route path="/" element = { <HomeScreen /> }></Route>
-        </Routes>
-        
-      </main>
-    </div>
+      <div className="d-flex flex-column site-container">
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container>
+              <LinkContainer to="/">
+                <Navbar.Brand>Amazona</Navbar.Brand>
+              </LinkContainer>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container>
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />}></Route>
+              <Route path="/" element={<HomeScreen />}></Route>
+            </Routes>
+          </Container>
+        </main>
+        <footer>
+          <div className="text-center">All rights reserved</div>
+        </footer>
+      </div>
     </BrowserRouter>
   );
 }
