cec7563e9bdf0ea1b5ad6a81edcb4f26f33cca5f
Vishal Kumar <vishalkumar2803001@gmail.com>
2022-06-07 19:47:09 +0530

    Added Basic error handling and cart functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,17 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-// import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import HomeScreen from "./screens/HomeSceen.js";
 import ProductScreen from "./screens/ProductScreen.js";
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import Badge from 'react-bootstrap/Badge';
 import Container from 'react-bootstrap/Container';
-import { LinkContainer } from 'react-router-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap';
+import { useContext } from "react";
+import { Store } from "./Store.js";
 
 function App() {
+  const { state } = useContext(Store);
+  const { cart } = state;
   return (
     <BrowserRouter>
       <div className="d-flex flex-column site-container">
@@ -16,11 +21,21 @@ function App() {
               <LinkContainer to="/">
                 <Navbar.Brand>Amazona</Navbar.Brand>
               </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">{cart.cartItems.length}</Badge>
+                  )
+
+                  }
+                </Link>
+              </Nav>
             </Container>
           </Navbar>
         </header>
         <main>
-          <Container>
+          <Container className="mt-3">
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />}></Route>
               <Route path="/" element={<HomeScreen />}></Route>
